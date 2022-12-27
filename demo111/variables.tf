800e9a3d7c7679186d6007042e4d1ff2c8331c9d
Kostya Linou <klinou@confluent.io>
2022-12-27 18:48:44 -0500

    terraform fmt

--- demo111/variables.tf
+++ demo111/variables.tf
@@ -13,14 +13,17 @@ variable "confluent_cloud_api_secret" {
 variable "topic_name" {
   description = "The name of the topic to create"
   type        = string
+  default     = "module_test"
 }
 
 variable "environment_id" {
   description = "The environment where the Kafka cluster exists"
   type        = string
+  default     = "env-9k5g5v"
 }
 
 variable "kafka_cluster_id" {
   description = "The ID of the Kafka cluster"
   type        = string
+  default     = "lkc-nwom9d"
 }
